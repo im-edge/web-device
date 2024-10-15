bd0d3b028321a1c1ebb4662e380fcbf9f5db53ca
Thomas Gelf <thomas@gelf.net>
2024-10-15 17:06:08 +0200

    RegisteredJack8P8C.: fix types

--- src/Widget/Pluggable/RegisteredJack8P8C.php
+++ src/Widget/Pluggable/RegisteredJack8P8C.php
@@ -7,6 +7,6 @@ class RegisteredJack8P8C extends RegisteredJack
     // 11.68mm x 8.30mm
     protected float $width = 11.68;
     protected float $height = 8.30;
-    protected $contactPositions = 8;
-    protected $contacts = 8;
+    protected int $contactPositions = 8;
+    protected int $contacts = 8;
 }
